e160f193cf682f9d9dca6bbf78500539670819fb
Gary O'Neall <gary@sourceauditor.com>
2023-02-09 12:12:50 -0800

    Update version for release 0.1.3
    Signed-off-by: Gary O'Neall <gary@sourceauditor.com>

--- src/main/java/org/spdx/cdx2spdx/CycloneToSpdx.java
+++ src/main/java/org/spdx/cdx2spdx/CycloneToSpdx.java
@@ -44,7 +44,7 @@ public class CycloneToSpdx {
     
     static final int ERROR_STATUS = 1;
     
-    static final String VERSION = "0.0.2";
+    static final String VERSION = "0.0.3";
     
     public enum SerFileType {
         JSON, RDFXML, XML, XLS, XLSX, YAML, TAG
